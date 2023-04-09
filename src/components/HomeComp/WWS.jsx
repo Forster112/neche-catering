cf4e1958f7c7cb5e4537b8a373b3cf92e5f1adb5
Forster112 <forstermichael112@gmail.com>
2023-04-09 18:38:58 +0100

    added images and a section

--- src/components/HomeComp/WWS.jsx
+++ src/components/HomeComp/WWS.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 
+import service01 from "../../assets/images/service-01.png";
+import service02 from "../../assets/images/service-02.png";
+import service03 from "../../assets/images/service-03.png";
 import "../../styles/wws.css";
 
 const WWS = () => {
@@ -23,9 +26,34 @@ const WWS = () => {
         </p>
       </div>
       <Row>
-        <Col lg="4" md="6" sm="12">n</Col>
-        <Col lg="4" md="6" sm="12">n</Col>
-        <Col lg="4" md="6" sm="12">t</Col>
+        <Col lg="4" md="6" sm="6">
+          <div className="wws__content d-flex">
+            <img
+              src={service01}
+              alt="delivery"
+              className="wws-content-image"
+            />
+            <h5 className="fw-bold mb-3">
+              Quick delivery
+            </h5>
+          </div>
+        </Col>
+        <Col lg="4" md="6" sm="6">
+          <div className="wws__content d-flex">
+            <img src={service02} alt="payment" />
+            <h5 className="fw-bold mb-3">
+              Easy payment
+            </h5>
+          </div>
+        </Col>
+        <Col lg="4" md="6" sm="6">
+          <div className="wws__content d-flex">
+            <img src={service03} alt="pickup" />
+            <h5 className="fw-bold mb-3">
+              Easy pickup
+            </h5>
+          </div>
+        </Col>
       </Row>
     </>
   );
